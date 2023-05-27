fbb0a658619c872ad1eba4e8516d6cad43f2a19f
klchen0112 <klchen0112@gmail.com>
2023-05-27 07:00:11 +0000

    add own charybdis keymap

---
+++ keyboards/bastardkb/charybdis/4x6/keymaps/klchen0112/rules.mk
@@ -0,0 +1,17 @@
+VIA_ENABLE=yes
+
+VIAL_ENABLE = yes
+
+MOUSEKEY_ENABLE = yes
+
+AUDIO_ENABLE = no          # Audio output
+
+SPLIT_KEYBOARD = yes       # Split common
+
+SRC += features/select_word.c
+
+# https://docs.qmk.fm/#/feature_caps_word?id=caps-word
+CAPS_WORD_ENABLE = yes
+
+# https://docs.qmk.fm/#/custom_quantum_functions?id=deferred-execution
+DEFERRED_EXEC_ENABLE = yes
