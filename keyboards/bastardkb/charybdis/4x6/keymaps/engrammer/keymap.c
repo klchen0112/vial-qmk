63424c973755751bcf1340c9d813d9f4926f62a8
klchen0112 <klchen0112@gmail.com>
2025-03-08 22:59:21 +0800

    add qwerty

--- keyboards/bastardkb/charybdis/4x6/keymaps/engrammer/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/engrammer/keymap.c
@@ -30,7 +30,7 @@ enum charybdis_keymap_layers {
     SYM,
     MOU,
     SYS,
-    QWERTY,
+    QWE,
 };
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
@@ -84,12 +84,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────┤
                 KC_LBRC,    KC_B,   KC_Y,   KC_O,   KC_U,   KC_QUOT,     KC_SCLN,   KC_L,   KC_D,   KC_W,   KC_V,          KC_RBRC,
   // ├───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────┤
-                KC_COMM,    LG_C,   LA_I,   LC_E,   LS_A,      KC_Z,        KC_Q,   RS_H,   RC_T,   RA_S,   RG_N,           KC_DOT,
+          KC_COMM,    LG_C,   LA_I,   LC_E,   LS_A,      KC_Z,        KC_Q,   RS_H,   RC_T,   RA_S,   RG_N,           KC_DOT,
   // ├───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────┤
                 KC_LSFT,    KC_G,   KC_X,   KC_J,   KC_K,   KC_MINS,     KC_SLSH,   KC_R,   KC_M,   KC_F,   KC_P,          KC_RSFT,
   // ╰───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────╯
-                          KC_TAB, LT(NUM,KC_LCTL), LT(NAV,KC_SLSH),     LT(FN,KC_ENTER),  LT(MOU,KC_BSPC),
-                                            KC_ESC,  LT(FN,KC_BSLS),     LT(SYM,KC_SPC)
+                          KC_TAB, LT(NUM,KC_LCTL),  LT(NAV,KC_SLSH),     LT(FN,KC_ENTER),  LT(MOU,KC_BSPC),
+                                  KC_ESC ,  LT(FN,KC_BSLS),     LT(SYM,KC_SPC)
   //                            ╰────────────────────────────────────╯ ╰──────────────────────────────╯
   ),
 
@@ -186,7 +186,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            KC_WAKE, KC_SLEP,    _______
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
-
+  [QWE] = LAYOUT(
+    // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+          KC_GRAVE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_LBRC,
+    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+         KC_MINS,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+         KC_PLUS,     KC_A,    KC_S, KC_D, KC_F,    KC_G,           KC_H,    LSFT_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LGUI_T(KC_SCLN), KC_QUOT,
+    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_COMM,
+    // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                     KC_TAB, KC_SPC,  XXXXXXX, KC_DEL,  KC_BSPC,
+                                             KC_ESC,    STLT ,     KC_ENTER
+    //                            ╰───────────────────────────╯ ╰──────────────────╯
+    ),
 };
 
 // const custom_shift_key_t custom_shift_keys[] = {
