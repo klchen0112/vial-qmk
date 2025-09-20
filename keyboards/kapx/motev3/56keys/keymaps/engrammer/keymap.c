d3d9b086c016bd72e7ff5f0705b8ecd483b128da
klchen <klchen0112@gmail.com>
2025-09-20 20:13:54 +0800

    update keymap

--- keyboards/kapx/motev3/56keys/keymaps/engrammer/keymap.c
+++ keyboards/kapx/motev3/56keys/keymaps/engrammer/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────┤
         KC_SLSH,            KC_G,   KC_X,    KC_J,  KC_K,   KC_MINS,      KC_EQL,   KC_R,   KC_M,   KC_F,    KC_P,         KC_BSLS,
   // ╰───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────╯
-                  KC_TAB, LT(NUM,KC_LCTL),   LT(FN,KC_ESC), DF(GAME),     KC_MUTE, LT(SYM,KC_SPC), LT(FN,KC_ENTER),  LT(MOU,KC_BSPC),
+                   LT(NUM,KC_TAB), KC_LCTL,   LT(FN,KC_ESC), DF(GAME),     KC_MUTE, LT(SYM,KC_SPC), LT(FN,KC_ENTER),  LT(MOU,KC_BSPC),
   //                            ╰────────────────────────────────────╯ ╰──────────────────────────────╯
                MO(2),   MO(3),   MO(1),    MO(4),   MO(5),      KC_ENT,  KC_RGHT,  KC_DOWN, KC_LEFT, KC_UP,
                             SYS_ESC, KC_1, RM_PREV,              RM_SPDU, KC_1, SYS_BSPC
