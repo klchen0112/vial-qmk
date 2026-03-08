ba6dff77ae865a9fec832b56a4db6b54ade7a64c
klchen <klchen0112@gmail.com>
2026-03-09 00:05:41 +0800

    add flake

--- flake.nix
+++ flake.nix
@@ -6,40 +6,52 @@
       let
         pkgs = import nixpkgs { inherit system; };
         # change to your liking
-        keyboard = "bastardkb/charybdis/4x6/v2/splinky_3";
+        keyboard_map = {
+          "motev3" = "kapx/motev3/56keys/round_3389";
+          "charybdis4x6" = "bastardkb/charybdis/4x6/v2/splinky_3";
+        };
         keymap = "engrammer";
-      in
-      {
-        # build with `nix build '.?submodules=1'`
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "keymap";
+
+        mkFirmware  = name: keyboard_path : pkgs.stdenv.mkDerivation {
+          pname = "qmk-firmware-${name}";
+          version = "0.1.0";
           src = ./.;
+
+          # 必须包含子模块
           phases = [ "buildPhase" ];
-          buildInputs = [ pkgs.qmk ];
+          # 必须包含子模块
+          submodules = true;
+
+          nativeBuildInputs = [ pkgs.qmk pkgs.gcc-arm-embedded pkgs.python3 ];
+
           buildPhase = ''
-            make -C $src BUILD_DIR=`pwd`/.build COPY=echo -j8 ${keyboard}:${keymap}
+            make -C $src BUILD_DIR=`pwd`/.build COPY=echo -j8 ${keyboard_path}:${keymap}
             mkdir $out
             cp -r .build/* $out/
           '';
         };
+        mkShell = name: path :
+        ''
+          build_${name}() {
+            qmk compile -kb ${path} -km engrammer
+          }
+          flash_${name}() {
+            qmk flash  -kb ${path} -km engrammer
+          }
+        '';
+      in
+      {
+        # build with `nix build '.?submodules=1'`
+        packages = builtins.mapAttrs (name: path: mkFirmware name path) keyboard_map;
 
         # Get a devshell with `nix develop` and then use `build` and `flash`.
         # Build dir is taken as a param of `build` and `flash`,
         # e.g. to flash with the output of `nix build '.?submodules=1'` do `flash result`.
         devShell = pkgs.mkShell {
-          KEYBOARD = keyboard;
-          KEYMAP = keymap;
           buildInputs = [ pkgs.qmk ];
-          shellHook = ''
-            build() {
-              BUILD_DIR=''${1:-.build}
-              make -C . BUILD_DIR=$BUILD_DIR COPY=echo -j8 $KEYBOARD:$KEYMAP
-            }
-            flash() {
-              BUILD_DIR=''${1:-.build}
-              make -C . BUILD_DIR=$BUILD_DIR COPY=echo -j8 $KEYBOARD:$KEYMAP:flash
-            }
-          '';
+          shellHook = pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (name: value:
+            mkShell name value
+          ) keyboard_map);
         };
       }
   );
