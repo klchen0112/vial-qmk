dfe334f4f154f6f8653edce6c9afdf1e589e8c71
klchen <klchen0112@gmail.com>
2026-03-29 01:00:25 +0800

    fix(engrammer): better shift

--- keyboards/kapx/motev3/56keys/keymaps/engrammer/keymap.c
+++ keyboards/kapx/motev3/56keys/keymaps/engrammer/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────┤
         KC_SLSH,            KC_G,   KC_X,    KC_J,  KC_K,   KC_MINS,      KC_EQL,   KC_R,   KC_M,   KC_F,    KC_P,         KC_BSLS,
   // ╰───────────────────────────────────────────────────────────────┤ ├───────────────────────────────────────────────────────────╯
-           XXXXXXX, LT(NUM,KC_TAB),   LT(FN, KC_ESC), DF(GAME),     KC_MUTE, LT(SYM,KC_ENTER), KC_SPC, LT(MOU,KC_BSPC),
+           KC_LSFT, LT(NUM,KC_TAB),   LT(FN, KC_ESC), DF(GAME),     KC_MUTE, LT(SYM,KC_ENTER), KC_SPC, LT(MOU,KC_BSPC),
   //                            ╰────────────────────────────────────╯ ╰──────────────────────────────╯
                MO(2),   MO(3),   MO(1),    MO(4),   MO(5),      KC_ENT,  KC_RGHT,  KC_DOWN, KC_LEFT, KC_UP,
                             MO(SYS), MO(NAV), RM_PREV,              RM_SPDU, QK_CAPS_WORD_TOGGLE, MO(SYS)
